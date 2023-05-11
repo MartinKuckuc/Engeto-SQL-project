3cc4073b3933ecc210a1fd958302290be81d7a51
MartinKuckuc <122486307+MartinKuckuc@users.noreply.github.com>
2023-05-11 22:57:10 +0200

    Update Vyzkumne_otazky.sql

--- Vyzkumne_otazky.sql
+++ Vyzkumne_otazky.sql
@@ -136,30 +136,36 @@ SELECT 31520.5/24.28;
 -- Vypocet pro rok 20018. Vysledek: 1728 litru mleka za rok 2018.
 SELECT 31520.5/18.24;
 
+
 -- 3. OTAZKA
 -- Ktera kategorie potravin zdrazuje nejpomaleji (je u ni nejnizsi percentualni mezirocni narust)?
--- Vytvoreni tabulky t_food_price
+-- Vytvoreni tabulky t_food_price, vytazeni informaci z primarni tabulky t_Martin_Kucirek_project_SQL_primary_final
+-- Novou tabulku si musim pripravit, abych mohl nize pojovat data pres LEF JOIN
+
 CREATE OR REPLACE TABLE t_food_price
 SELECT
 	food_name AS food_name_2,
-	average_price AS average_price_2,
+	average_food_price AS average_food_price_2,
 	year_of_measurement AS year_of_measurement_2
 FROM t_Martin_Kucirek_project_SQL_primary_final
-WHERE average_price IS NOT NULL;
+WHERE average_food_price IS NOT NULL;
 
 -- Zobrazeni tabulky t_food_price
 SELECT *
 FROM t_food_price;
 
 
-
 -- Vytvoreni tabulky t_food_price_change
+-- Data musim brat z jine tabulky abych novou tabulku mohl propojit s tabulkou primary 
+-- Spojeni PRIMARNI! tabulky (t_Martin_Kucirek_project_SQL_primary_final), ze které vycházím a t_food_price
+-- Pres prikaz LEFT JOIN ON year_of_measurement = year_of_measurement_2 - 1
+
 CREATE OR REPLACE TABLE t_food_price_change
 SELECT 
 	food_name,
 	food_name_2,
-	average_price,
-	average_price_2,
+	average_food_price,
+	average_food_price_2,
 	year_of_measurement, 
 	year_of_measurement_2
 FROM t_Martin_Kucirek_project_SQL_primary_final
@@ -167,17 +173,26 @@ LEFT JOIN t_food_price
 	ON t_Martin_Kucirek_project_SQL_primary_final.year_of_measurement = t_food_price.year_of_measurement_2 - 1
 WHERE food_name = food_name_2 
 GROUP BY food_name, food_name_2, year_of_measurement;
-	
 
--- Zobrazeni tabulky t_food_price_change
-SELECT *
-FROM t_food_price_change;
 
--- Vypocet mezirocniho narustu
-SELECT *,
-	round((average_price_2 - average_price) / average_price_2 * 100, 2 ) AS price_growth
-FROM t_food_price_change
+-- Vypocet mezirocniho narustu pomoci klauzule round
+-- Odpoved na otazku a je generovana z hlavni tabulky primary! (t_Martin_Kucirek_project_SQL_primary_final)
+
+SELECT 
+	round((t_food_price.average_food_price_2 - t_Martin_Kucirek_project_SQL_primary_final.average_food_price) / t_food_price.average_food_price_2 * 100, 2 ) AS price_growth,
+	food_name,
+	food_name_2,
+	average_food_price,
+	average_food_price_2,
+	year_of_measurement, 
+	year_of_measurement_2
+FROM t_Martin_Kucirek_project_SQL_primary_final
+LEFT JOIN t_food_price
+	ON t_Martin_Kucirek_project_SQL_primary_final.year_of_measurement = t_food_price.year_of_measurement_2 - 1
+WHERE t_Martin_Kucirek_project_SQL_primary_final.food_name = t_food_price.food_name_2 
+GROUP BY food_name, food_name_2, year_of_measurement
 ORDER BY price_growth ASC;
+
 
 -- ODPOVED (3/5): 
 -- Mnoho potravin mezirocne zlevnilo. Jedna se celkem o 119 polozek ve srovnavacim obdobi mezi lety 2006-2018.
@@ -187,28 +202,9 @@ ORDER BY price_growth ASC;
 
 
 -- 4. OTAZKA  
--- Spojeni dvou tabulek czechia_payroll (cpay) a czechia_payroll_industry_branch (cpib) pres "code" a vytvoreni tabulky t2.
--- Vytvoreni tabulky t_payroll_1
+-- Existuje rok, ve kterem byl mezirocni narust cen potravin vyrazne vyssi nez rust mezd (vetsi nez 10%)?
 
-CREATE OR REPLACE TABLE t_payroll_year_1 AS
-SELECT payroll_year, average_salary
-FROM (
-	SELECT cpib.name AS industry_name, avg(cpay.value) AS average_salary, cpay.payroll_year
-	FROM czechia_payroll cpay
-	LEFT JOIN czechia_payroll_industry_branch cpib
-	ON cpay.industry_branch_code = cpib.code
-	WHERE cpay.value_type_code = 5958
-		AND cpay.calculation_code = 100	
-		AND cpay.unit_code = 200
-		AND cpay.industry_branch_code IS NOT NULL
-	GROUP BY cpay.payroll_year
-	ORDER BY cpay.payroll_year) t2;
-	
-
-SELECT *
-FROM t_payroll_year_1;
-
--- Vytvoreni tabulky t_payroll_year_2
+-- Vytvoreni si nove tabulky pro ucely 4. ukolu - tabulka t_payroll_year_2 
 CREATE OR REPLACE TABLE t_payroll_year_2 AS
 SELECT payroll_year AS payroll_year_2, average_salary AS average_salary_2
 FROM (
@@ -222,29 +218,29 @@ FROM (
 		AND cpay.industry_branch_code IS NOT NULL
 	GROUP BY cpay.payroll_year
 	ORDER BY cpay.payroll_year) t2;
-	
 
 SELECT *
 FROM t_payroll_year_2;
 
--- Spojeni tabulek t_payroll_year_1 a t_payroll_year_2
+-- Spojeni PRIMARNI tabulky t_Martin_kucirek_project_SQL_primary_final (ze které vycházím!) a t_payroll_year_2
 -- Vytvoreni nove tabulky t_salary_growth
 CREATE OR REPLACE TABLE t_salary_growth
 SELECT payroll_year, payroll_year_2, average_salary, average_salary_2, 
 	round((average_salary_2 - average_salary) / average_salary_2 * 100, 2 ) AS salary_growth
-FROM t_payroll_year_1
+FROM t_Martin_kucirek_project_SQL_primary_final
 LEFT JOIN t_payroll_year_2
 	ON payroll_year = payroll_year_2 -1
 ORDER BY payroll_year ASC;
-	
+
 
 SELECT *
 FROM t_salary_growth;
 
-
 -- Vpocet mezirocnimu rustu cen u potravin.
+-- K vypoctu nam pomohla tabulka ze 3. ukolu: t_food_price_change
+-- Tato tabulka generuje odpovedi z PRIMARNI! tabulky.
 SELECT *,
-	round((average_price_2 - average_price) / average_price_2 * 100, 2 ) AS price_growth
+	round((average_food_price_2 - average_food_price) / average_food_price_2 * 100, 2 ) AS price_growth
 FROM t_food_price_change
 GROUP BY year_of_measurement 
 ORDER BY year_of_measurement ASC;
@@ -254,7 +250,7 @@ CREATE OR REPLACE TABLE t_difference_salary_food_price AS
 SELECT *
 FROM (
 	SELECT food_name, year_of_measurement, 
-		round((average_price_2 - average_price) / average_price_2 * 100, 2 ) AS price_growth
+		round((average_food_price_2 - average_food_price) / average_food_price_2 * 100, 2 ) AS price_growth
 	FROM t_food_price_change) t1
 LEFT JOIN (
 	SELECT payroll_year, salary_growth
@@ -277,43 +273,38 @@ ORDER BY rozdil DESC;
 -- Zadny takovy rok neni, kde by byl mezirocni narut cen potrvin vyrazne vyssi nez rust mezd. 
 
 
-
 -- 5. OTAZKA:
 -- Ma vyska HDP vliv na zmeny ve mzdach a cenach potravin? Neboli, pokud HDP vzroste vyrazneji v jednom roce, projevi se to na cenach potravin 
 -- ci mzdach ve stejnem nebo nasdujícím roce vyraznějším rustem?
 
--- Vyuziju druhe tabulky t_martin_kucirek_project_sql_secondary_final a prejmenuju si ji pro ucely 5. otazky na t_differences_in_salary_and_prices. 
+-- Vyuziji sekundarni tabulky t_martin_kucirek_project_sql_secondary_final a vytvorim si view, ktery upravuji 
 -- Pridam podminku pro Czech Republic.
--- Vyuziti vypoctu pomoci lag - OVER. Klauzule pouzije udaje z predchoziho radku srovnano podle YEAR a vypocita se change_of_gdp.
-CREATE OR REPLACE TABLE t_differences_in_salary_and_prices AS 
-SELECT country_countries, country_economy, t2.YEAR, t2.gdp, t2.gini, t2.population,
-	round(((gdp - (lag(gdp) OVER (ORDER BY `YEAR`))) / (lag(gdp) OVER (ORDER BY `YEAR`)) * 100), 2) AS change_of_gdp
-FROM (	
-	SELECT c.country AS country_countries, c.continent
-	FROM countries c 
-	WHERE c.country = 'Czech Republic') t1
-LEFT JOIN (
-	SELECT e.country AS country_economy, e.population, e.YEAR, e.gdp, e.gini
-	FROM economies e
-	WHERE YEAR >= 2000 ) t2 
-	ON country_countries = country_economy;
+-- Vyuziju vypoctu pomoci lag - OVER. Klauzule pouzije udaje z predchoziho radku srovnano podle YEAR a vypocita se change_of_gdp.
 
--- Vyuziti vypoctu pomoci lag - OVER. Klauzule pouzije udaje z predchoziho radku srovnano podle YEAR a vypocita se change_of_gdp.
+CREATE OR REPLACE VIEW v_martin_kucirek_project_sql_secondary_final AS 
+SELECT *, round(((gdp - (lag(gdp) OVER (ORDER BY `YEAR`))) / (lag(gdp) OVER (ORDER BY `YEAR`)) * 100), 2) AS change_of_gdp
+FROM t_martin_kucirek_project_sql_secondary_final
+WHERE country_countries = 'Czech Republic';
 
+-- Zobrazeni nového view ze sekundarni tabulky!
+SELECT *
+FROM v_martin_kucirek_project_sql_secondary_final;
+
+-- Tabulka ze 4.ukolu:
 SELECT *
 FROM t_differences_in_salary_and_prices;
 
 
--- Tabulka ze 4.ukolu t_difference_salary_food_price.
+-- Tabulka ze 4.ukolu t_difference_salary_food_price obohacena o dalsi vypocet
 CREATE OR REPLACE TABLE t_difference_salary_food_price AS 
 SELECT *,
 	price_growth - salary_growth AS difference_price_and_salary
 FROM (
 	SELECT food_name, year_of_measurement, 
-	round((average_price_2 - average_price) / average_price_2 * 100, 2 ) AS price_growth
+	round((average_food_price_2 - average_food_price) / average_food_price_2 * 100, 2 ) AS price_growth
 	FROM t_food_price_change) t1
 LEFT JOIN (
-	SELECT payroll_year, industry_name, salary_growth
+	SELECT payroll_year, salary_growth
 	FROM t_salary_growth) t2
 	ON year_of_measurement = payroll_year;
 
@@ -329,7 +320,7 @@ JOIN t_differences_in_salary_and_prices
 	ON t_differences_in_salary_and_prices.year = t_difference_salary_food_price.year_of_measurement;
 
 -- Filtrovani zobrazovacich dat:
-SELECT year_of_measurement, country_countries, food_name, industry_name, 
+SELECT year_of_measurement, country_countries, food_name,  
 	change_of_gdp, price_growth, salary_growth, difference_price_and_salary
 FROM t_difference_salary_food_price
 JOIN t_differences_in_salary_and_prices
@@ -337,4 +328,5 @@ JOIN t_differences_in_salary_and_prices
 
 -- ODPOVED (5/5):
 -- Podle vysledku z tabulky nema primy vliv na cenu veskereho zbozi rust mezd jednotlivych odvetvi anebo narust gdp.
+
 
